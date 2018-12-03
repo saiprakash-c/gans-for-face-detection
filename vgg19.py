9775790c55893016d0c2dc9005bd669b1cd08231
saiprakash-c <43300703+saiprakash-c@users.noreply.github.com>
2018-12-02 23:42:02 -0500

    Add files via upload

---
+++ vgg19.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import os
+
+import numpy as np
+import inspect
+
+"""
+vgg19_npy_path = None
+
+if vgg19_npy_path is None:
+    path = inspect.getfile(vgg19)
+    path = os.path.abspath(os.path.join(path, os.pardir))
+    path = os.path.join(path, "vgg19.npy")
+    vgg19_npy_path = path
+    print(vgg19_npy_path)
+
+
+"""
+
+data_dict = np.load('vgg19.npy', encoding='latin1').item()
+
+
+
+def get_conv_filter(self, name):
+    return tf.constant(self.data_dict[name][0], name="filter")
+
+
+def get_bias(self, name):
+    return tf.constant(self.data_dict[name][1], name="biases")
+
+
+def get_fc_weight(self, name):
+    return tf.constant(self.data_dict[name][0], name="weights")
